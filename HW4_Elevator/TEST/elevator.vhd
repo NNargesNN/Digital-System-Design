7594e40cfa64c2c54c5b2a296146f8aab2a662ca
Narges <ghasemi.narges2000@gmail.com>
2021-05-11 22:39:26 +0430

    HW4

--- HW4_Elevator/TEST/elevator.vhd
+++ HW4_Elevator/TEST/elevator.vhd
@@ -34,23 +34,24 @@ ARCHITECTURE Behavioral of elevator is
 Signal temp_state : std_logic_vector (n-1 DOWNTO 0):="0001";
    SIGNAL next_state : state;
 	SIGNAL door_state : STD_LOGIC := '1'; --1 open 0 close
-	Signal last_motor_movement : STD_LOGIC_VECTOR(1 DOWNTO 0) := "00";
+	--Signal last_motor_movement : STD_LOGIC_VECTOR(1 DOWNTO 0) := "00";
 	Signal steady_floor  : std_logic_vector (n-1 DOWNTO 0):="0001";
-
+	Signal motor_up_tmp,motor_down_tmp :STD_LOGIC;
+	Signal close_door_motor_tmp,open_door_motor_tmp : STD_LOGIC;
 
 BEGIN
 	
-
+--	current_floor <= log2 switch
 	
 	com : PROCESS (clk)
-		--VARIABLE last_motor_movement : STD_LOGIC_VECTOR(1 DOWNTO 0) := "00";
+		VARIABLE last_motor_movement : STD_LOGIC_VECTOR(1 DOWNTO 0) := "00";
 		
 		
 		BEGIN
 		IF current_state = STEADY_STATE THEN 
-			motor_up<= '0';
-			motor_down<='0';
-			last_motor_movement<="00";
+			motor_up_tmp<= '0';
+			motor_down_tmp<='0';
+			last_motor_movement:="00";
 			steady_floor<= temp_state;
 			IF door_open='1' THEN
 				next_state<=DOOR_OPEN_STATE;  
@@ -60,8 +61,8 @@ BEGIN
 		
 		ELSIF current_state = DOOR_OPEN_STATE THEN 
 			IF come ="0000" AND go="0000" THEN
-			close_door_motor<='0';
-			open_door_motor<='1';
+			close_door_motor_tmp<='0';
+			open_door_motor_tmp<='1';
 			next_state<=DOOR_OPEN_STATE;
 			ELSIF ((come="0000" AND ((go AND switch) =switch )) OR (go="0000" AND ((come AND switch)=switch ))) THEN
 				next_state<=DOOR_OPEN_STATE;
@@ -78,20 +79,20 @@ BEGIN
 			
 		ELSIF current_state = DOOR_CLOSE_STATE THEN 
 			next_state <=MOVE_STATE;
-			close_door_motor<='1';
-			open_door_motor<='0';
+			close_door_motor_tmp<='1';
+			open_door_motor_tmp<='0';
 			
 		ELSIF current_state = WAIT_CLOSE_STATE THEN 
 			next_state <=DOOR_CLOSE_STATE;
-			open_door_motor<='0';
+			open_door_motor_tmp<='0';
 			
 		ELSIF current_state = WAIT_OPEN_STATE THEN 
 			next_state <=DOOR_OPEN_STATE;
-			close_door_motor<='0';
+			close_door_motor_tmp<='0';
 			
 		ELSIF current_state = WAIT_OPEN_STATE THEN 
 			next_state <=DOOR_OPEN_STATE;
-			close_door_motor<='0';
+			close_door_motor_tmp<='0';
 		
 		ELSIF current_state = MOVE_STATE and clk = '1' AND clk'event THEN
 			IF  ((come AND switch )=switch  OR (go AND switch) =switch ) AND( (steady_floor AND switch)/= switch )THEN
@@ -99,8 +100,8 @@ BEGIN
 			ELSE
 				IF (come>"0000" AND go>"0000") AND((come>switch AND go<switch) OR (come<switch AND go>switch)) THEN
 					IF last_motor_movement="10" THEN
-						motor_up<='1';
-						motor_down<='0';
+						motor_up_tmp<='1';
+						motor_down_tmp<='0';
 						temp_state <= temp_state(n-2 downto 0)&'0';
 						
 						IF (temp_state(n-2 downto 0)&'0')=come or (temp_state(n-2 downto 0)&'0')=go  THEN
@@ -109,8 +110,8 @@ BEGIN
 						next_state<=MOVE_STATE;
 						END IF;
 					ELSE
-						motor_up<='0';
-						motor_down<='1';
+						motor_up_tmp<='0';
+						motor_down_tmp<='1';
 						
 						temp_state <= '0'&temp_state(n-1 downto 1);
 						IF ('0'&temp_state(n-1 downto 1))=come or ('0'&temp_state(n-1 downto 1))=go  THEN
@@ -120,9 +121,9 @@ BEGIN
 						END IF;
 					END IF;
 				ELSIF (come >= switch AND go >= switch) OR (come > switch AND go = "0000")  OR (come = "0000" AND go > switch) THEN	
-					motor_up<='1';
-					motor_down<='0';
-					last_motor_movement<="10";
+					motor_up_tmp<='1';
+					motor_down_tmp<='0';
+					last_motor_movement:="10";
 					
 					temp_state <= temp_state(n-2 downto 0)&'0';
 					IF (temp_state(n-2 downto 0)&'0')=come or (temp_state(n-2 downto 0)&'0')=go  THEN
@@ -132,16 +133,12 @@ BEGIN
 					END IF;
 			    
 				ELSIF (come <= switch AND go <= switch) OR (come < switch AND go = "0000")  OR (come = "0000" AND go < switch) THEN	
-					motor_up<='0';
-					motor_down<='1';
-					last_motor_movement<="01";
+					motor_up_tmp<='0';
+					motor_down_tmp<='1';
+					last_motor_movement:="01";
 					
 					temp_state <= '0' &temp_state(n-1 downto 1);	
-					IF ('0' &temp_state(n-1 downto 1))=come or ('0' & temp_state(n-1 downto 1))=go  THEN
-						next_state<=STEADY_STATE;
-					ELSE
 					next_state<=MOVE_STATE;
-					END IF;
 			    END IF;
 				
 					
@@ -161,17 +158,18 @@ BEGIN
             current_state <= DOOR_OPEN_STATE;
 	    current_floor<="0001";
 	    elevator_state<= '0'& "0001";	
-	--    close_door_motor<='0';
-        --    open_door_motor<='1';
-		motor_up<='0';
-		motor_down<='0';
+---	    close_door_motor<='0';
+---            open_door_motor<='1';
         ELSIF clk = '1' AND clk'event THEN
             current_state <= next_state;
         END IF;
 	current_floor<=temp_state;
-	
+	--elevator_state<= '0'& temp_state;
 
-	
+	motor_up<=motor_up_tmp;
+	motor_down<=motor_down_tmp;
+	close_door_motor<= close_door_motor_tmp;
+	open_door_motor<=open_door_motor_tmp;
 
 	fan_out<=fan_in;
 	IF next_state=MOVE_STATE or current_state=MOVE_STATE THEN
